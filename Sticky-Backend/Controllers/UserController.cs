2ac4356b5007dd62218180a41ce671d9d3e0f353
Joel May <joel.may@live.com>
2017-04-11 14:35:00 -0500

    User validation controller stub

---
+++ Sticky-Backend/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sticky_Backend.Controllers
+{
+    class UserController
+    {
+        public static bool Validate(string username, string password)
+        {
+            // TODO: stub
+            Random r = new Random();
+            return r.Next() % 2 == 0;
+        }
+    }
+}
