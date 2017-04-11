2e48f65b2aa846aeb330d3861c61e424f8e78871
Joel May <joel.may@live.com>
2017-04-11 14:50:45 -0500

    User controller create stub & docs

--- Sticky-Backend/Controllers/UserController.cs
+++ Sticky-Backend/Controllers/UserController.cs
@@ -8,7 +8,28 @@ namespace Sticky_Backend.Controllers
 {
     class UserController
     {
+        /// <summary>
+        /// Validates a user's credentials.
+        /// </summary>
+        /// <param name="username">The username, the primay key.</param>
+        /// <param name="password">The user's password.</param>
+        /// <returns>True if the user account info is valid.</returns>
         public static bool Validate(string username, string password)
+        {
+            // TODO: stub
+            Random r = new Random();
+            return r.Next() % 2 == 0;
+        }
+
+        /// <summary>
+        /// Creates a new user in the database.
+        /// </summary>
+        /// <param name="username">The username primary key.</param>
+        /// <param name="avatar">The email address to locate a gravatar.</param>
+        /// <param name="name">The legal (or illegal) name for the user.</param>
+        /// <param name="password">The user's desired password.</param>
+        /// <returns>True if the user was created.</returns>
+        public static bool CreateUser(string username, string avatar, string name, string password)
         {
             // TODO: stub
             Random r = new Random();
